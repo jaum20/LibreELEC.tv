c37c7adb81f0e6deded5c694501ad10196e9c4a1
Alex Bee <knaerzche@gmail.com>
2020-09-11 10:51:44 +0200

    REBASE: Rockchip add support for RK322x

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,12 +2,13 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-4563e24
-PKG_VERSION="4563e249a3f47e7fcd47a4c3769b6c05683b6e9d"
-PKG_SHA256="0b3479117700bce9afea2110c1f027b626c76d99045802218b35a53606547d60"
+
+# Version is: knaerzche/tag:libreelec-991d0e4
+PKG_VERSION="991d0e441c74f93813e6a2b7e8d6ef8a136985ad"
+PKG_SHA256="5c13f1ca7beb6e96cccb76e03954075888673009464171faf954bbe91f48c732"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
-PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/knaerzche/rkbin/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
 PKG_TOOLCHAIN="manual"
